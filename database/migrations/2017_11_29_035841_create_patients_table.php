1583cc7d66d36b7ca364686d5a7ea27e1c245ba6
Gregory Dodd <gdodd86@gmail.com>
2017-11-29 01:38:07 -0600

    Created a migration to create a table to hold the Patient models

---
+++ database/migrations/2017_11_29_035841_create_patients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('patientFirstName');
+            $table->string('patientLastName');
+            $table->string('patientDateOfBirth');
+            $table->string('phoneNumber');
+            $table->string('memberId');
+            $table->tinyInteger('status');
+            $table->string('questionnaireFile')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
